a7ec23e3160d376969c5970024a53534e8a08615
aialok <alokgupta1560@gmail.com>
2023-08-14 13:50:07 +0530

    some improvement in bot.js

--- bot.js
+++ bot.js
@@ -130,8 +130,30 @@ bot.hears("Semester 2", (ctx) => {
   ctx.reply("Coming Soon....");
 })
 
+export default {
+  async fetch(request, env, ctx) {
+      if(request.method === "POST"){
+const payload = await request.json();
+          if('message' in payload){
+  const chatId = payload.message.chat.id;
+  const input = String(payload.message.text);
+  const user_firstname = String(payload.message.from.first_name);
+  const response = user_firstname + " said " + input;
+              await this.sendMessage(env.API_KEY, chatId, response);
+          }
+      }
+      return new Response('OK');
+  },
 
+  async sendMessage(apiKey, chatId, text){
+const url = `https://api.telegram.org/bot${process.env.botToken}/sendMessage?chat_id=${chatId}&text=${text}`;
+const data = await fetch(url).then(resp => resp.json());
+    console.log(data);
+  }
 
+};
+
+fetch();
 // Start the web server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
